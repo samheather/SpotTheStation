37f06801e72a2d65f9b7cbbf4d139f0b5472fe2e
Sam Heather <sam@heather.sh>
2014-07-20 11:54:47 +0100

    Indicator text in the middle starting to work

--- app/app/src/main/java/com/example/alessio/spotthestation/MainActivity.java
+++ app/app/src/main/java/com/example/alessio/spotthestation/MainActivity.java
@@ -62,6 +62,7 @@ public class MainActivity extends Activity implements LocationListener {
     private static double ISSLongitude = 0;
     private static double ISSLatitude = 0;
     private static long ISSNextTime = 0;
+    private static long ISSNextDuration = 0;
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -142,14 +143,19 @@ public class MainActivity extends Activity implements LocationListener {
         long unixTime = System.currentTimeMillis() / 1000L;
         long timeToShow = ISSNextTime - unixTime;
 
-        // Convert to String
-        Date date = new Date(timeToShow*1000L);
-        // *1000 is to convert seconds to milliseconds
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss"); // the format of your date
-        String formattedDate = sdf.format(date);
-        System.out.println(formattedDate);
+        if (unixTime - ISSNextTime > ISSNextDuration) {
+            // Convert to String
+            Date date = new Date(timeToShow * 1000L);
+            // *1000 is to convert seconds to milliseconds
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss"); // the format of your date
+            String formattedDate = sdf.format(date);
+            System.out.println(formattedDate);
 
-        timeText.setText(formattedDate);
+            timeText.setText(formattedDate);
+        }
+        else {
+            timeText.setText("Look Up!");
+        }
     }
 
     /**
@@ -230,7 +236,7 @@ public class MainActivity extends Activity implements LocationListener {
         latitude = location.getLatitude();
         longitude = location.getLongitude();
 
-        System.out.println("Lat: " + latitude + ", Longitude: " + longitude);
+        System.out.println("User: Lat: " + latitude + ", Longitude: " + longitude);
 
         System.out.println(updateElevation(longitude, latitude, 165, 15).getDegsHeading());
 
@@ -295,5 +301,13 @@ public class MainActivity extends Activity implements LocationListener {
 
     public static void setISSNextTime(long newNextTime) {
         MainActivity.ISSNextTime = newNextTime;
+    }
+
+    public static long getISSNextDuration() {
+        return MainActivity.ISSNextDuration;
+    }
+
+    public static void setISSNextDuration(long newNextDuration) {
+        MainActivity.ISSNextDuration = newNextDuration;
     }
 }
